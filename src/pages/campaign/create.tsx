ff77159713170c8c13865c5b36b3baff8ce2fa6c
junki-dev <kjgcde@gmail.com>
2023-02-12 20:34:14 +0900

    feat: period, goal amount 라디오 그룹 추가

--- src/pages/campaign/create.tsx
+++ src/pages/campaign/create.tsx
@@ -8,13 +8,22 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import { addCampaign } from '../../api/CampaignAPI'
 import { ethers } from 'ethers'
 import { Contract } from '../../utils/Contract'
-import { Input } from '@mui/material'
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 
 type Inputs = {
   title: string
   description: string
   writerAddress: string
+  goalAmount: string
+  period: string
   img1: FileList
   img2: FileList
   img3: FileList
@@ -31,6 +40,8 @@ const CampaignCreate = () => {
       title: '',
       description: '',
       writerAddress: '',
+      goalAmount: '',
+      period: '',
       img1: undefined,
       img2: undefined,
       img3: undefined,
@@ -46,13 +57,14 @@ const CampaignCreate = () => {
     formData.append('img2', data.img2[0])
     formData.append('img3', data.img3[0])
 
+    console.log(data)
+
     await addCampaign(formData).catch((err) => {
       alert(err)
       return null
     })
 
     // call contract
-
     const provider = await Contract.getProvider()
     // calculate gas price
     const price = ethers.utils.formatUnits(
@@ -86,6 +98,62 @@ const CampaignCreate = () => {
         label='writerAddress'
         register={register('writerAddress')}
       />
+      <FormControl>
+        <FormLabel id='radio-group-goal-label'>Goal</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby='radio-group-goal-label'
+          defaultValue='10'
+          name='radio-group-goal'
+        >
+          <FormControlLabel
+            value='10'
+            control={<Radio />}
+            label='10$'
+            {...register('goalAmount')}
+          />
+          <FormControlLabel
+            value='100'
+            control={<Radio />}
+            label='100$'
+            {...register('goalAmount')}
+          />
+          <FormControlLabel
+            value='1000'
+            control={<Radio />}
+            label='1000$'
+            {...register('goalAmount')}
+          />
+        </RadioGroup>
+      </FormControl>
+      <FormControl>
+        <FormLabel id='radio-group-period-label'>Period</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby='radio-group-period-label'
+          defaultValue='0'
+          name='radio-group-period'
+        >
+          <FormControlLabel
+            value='0'
+            control={<Radio />}
+            label='2 weeks'
+            {...register('period')}
+          />
+          <FormControlLabel
+            value='1'
+            control={<Radio />}
+            label='1 Month'
+            {...register('period')}
+          />
+          <FormControlLabel
+            value='2'
+            control={<Radio />}
+            label='3 Month'
+            {...register('period')}
+          />
+        </RadioGroup>
+      </FormControl>
       <BaseInput
         name='img1'
         type='file'
@@ -120,7 +188,6 @@ export default CampaignCreate
 
 const Form = styled.form`
   width: 100vw;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
